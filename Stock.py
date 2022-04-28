898082a168da692a06d8009ad7808e342fc0f64b
jpatrick5402 <jpatrick5402@gmail.com>
2022-04-28 03:22:31 -0400

    Letting Stack.py print moar

--- Stock.py
+++ Stock.py
@@ -2,6 +2,7 @@
 #from xml.sax import xmlreader
 import requests
 import json
+import math
 
 URL = "https://www.google.com/search?q=microsoft+stock+price+5+days&rlz=1C1CHBF_enUS962US962&sxsrf=ALiCzsa2kbtP_BMPN3CT1bOnI-__sx6-Eg%3A1651122832916&ei=kCJqYuq8N8ib_QbclZCwDA&ved=0ahUKEwjqxL7v_7X3AhXITd8KHdwKBMYQ4dUDCA4&uact=5&oq=microsoft+stock+price+5+days&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCrAjoHCCMQsAMQJzoHCAAQRxCwAzoHCAAQsAMQQzoLCAAQgAQQsQMQgwE6BQgAEIAEOgYIABAWEB46BQgAEIYDOggIIRAWEB0QHkoECEEYAEoECEYYAFDxDVieJ2CIKmgBcAF4AIABeogB5gSSAQM2LjGYAQCgAQHIAQrAAQE&sclient=gws-wiz"
 page = requests.get(URL)
@@ -12,5 +13,12 @@ for index in formatted:
     if "BNeawe iBp4i AP7Wnd" in index:
         h=index.split(">")
         if h[-1] != "":
-            price = print(h[-1].strip())
-        
+            print(h[-1].strip())
+            price = h[-1].strip()
+changes = []
+for index in formatted:
+    if "UMOHqf fePwtd" in index:
+        print(index[0:-8].split(">")[-1])
+        changes.append(index[0:-8].split(">")[-1])
+
+price = float(price)
